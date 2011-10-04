ae276ba3cd3b0afaaa1df889a75186740a78abb8
Brian Connatser <connatser@gmail.com>
2011-10-04 01:44:51 -0400

    Consolidate minimizer config into single _assets.yml

--- lib/hera/minimize_hera.rb
+++ lib/hera/minimize_hera.rb
@@ -4,8 +4,7 @@ class MinimizeHera
   def initialize
 
     puts "Hera will minimize your css and js scripts."
-    system "jammit -o _public/css -c _css_jammit.yml"
-    system "jammit -o _public/js -c _js_jammit.yml"
+    system "jammit -o _public/assets -c _assets.yml"
     puts "Your production assets are now minimized."
 
   end
